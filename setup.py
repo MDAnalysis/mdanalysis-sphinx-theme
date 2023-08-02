94b43a010b5a6c33750ff3142eb876b1c908dba7
Ian Kenney <ianmichaelkenney@gmail.com>
2023-08-02 14:38:57 -0700

    Include MDA assets on pip install

--- setup.py
+++ setup.py
@@ -1,7 +1,46 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from pathlib import Path
+import shutil
 import versioneer
+
+
+def copy_mda_assets():
+    static_dir = Path('.') / 'mdanalysis_sphinx_theme' / 'static'
+    branding_dir = Path('.') / 'branding'
+
+    # outdir for logos and icons
+    static_logos = static_dir / 'logo'
+
+    # main logo to appear in nav bar
+    mda_logo: Path = branding_dir / 'logos' /\
+                                    'rastered' /\
+                                    'mdanalysis-logo_bgwhite@600ppi.png'
+
+    mda_favicon = branding_dir / 'logos' /\
+                                 'icons' /\
+                                 'mdanalysis-logo.ico'
+
+    if not mda_logo.exists():
+        raise FileNotFoundError("Could not find the MDAnalysis logo. "
+                                "Check that the branding submodule is "
+                                "initialized.")
+
+    if not mda_favicon.exists():
+        raise FileNotFoundError("Could not find the MDAnalysis icon. "
+                                "Check that the branding submodule is "
+                                "initialized.")
+
+    if not static_dir.exists():
+        raise FileNotFoundError("Could not find the theme static directory.")
+
+    shutil.copy(mda_logo, static_logos / 'mda_logo.png')
+    shutil.copy(mda_favicon, static_logos / 'mda_favicon.ico')
+
+
+copy_mda_assets()
+
 
 setup(
     name='mdanalysis_sphinx_theme',
@@ -19,6 +58,7 @@ setup(
         '*.html',
         'static/css/*.css',
         'static/js/*.js',
+        'static/logo/*',
         'sass/*.*',
         'sass/*/*.*',
         'sass/bulma/sass/*/*.*',
@@ -37,7 +77,7 @@ setup(
         'Topic :: Software Development :: Documentation',
     ],
     install_requires=['sphinx_rtd_theme'],
-    entry_points = {
+    entry_points={
         'sphinx.html_themes': [
             'mdanalysis_sphinx_theme = mdanalysis_sphinx_theme',
         ]
