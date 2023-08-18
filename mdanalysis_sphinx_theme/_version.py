f8a46d49b5437bf9f384985dc0a1c57d683cd50c
Lily Wang <lily@macaque.local>
2023-08-18 15:33:07 +1000

    update version prefix

--- mdanalysis_sphinx_theme/_version.py
+++ mdanalysis_sphinx_theme/_version.py
@@ -51,7 +51,7 @@ def get_config() -> VersioneerConfig:
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "mdanalysis_sphinx_theme-"
     cfg.versionfile_source = "mdanalysis_sphinx_theme/_version.py"
     cfg.verbose = False
