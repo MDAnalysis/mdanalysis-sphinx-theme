13561c14809d96749a7e900bf6349c51c236532c
Lily Wang <lily@mdanalysis.org>
2023-09-21 10:38:19 +0100

    pin sphinx in setup.py

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     python_requires='>=3.9',
     install_requires=[
         'sphinx_rtd_theme>=1.2',
-        'sphinx>=6.2.1',
+        'sphinx>=6.2.1,<7.0',
         'beautifulsoup4',
         'python-slugify[unidecode]',
         'css_html_js_minify',
