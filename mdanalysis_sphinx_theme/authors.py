8550b94e6cdca6c030e0fdf3333ea873e2ce2956
Lily Wang <lily@mdanalysis.org>
2023-12-14 18:15:01 +1100

    more documentation examples

---
+++ mdanalysis_sphinx_theme/authors.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+
+# This file is generated from the AUTHORS file during the installation process.
+# Do not edit it as your changes will be overwritten.
+
+__authors__ = [
+    u"Lily Wang",
+    u"Irfan Alibay",
+    u"Ian Kenney",
+]
